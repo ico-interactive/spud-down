718d64e3c28c57514dc353dbbfb10b60768d5cb5
dokxid <dan-du@live.de>
2026-06-20 17:43:28 +0200

    feat: add functional slash command and general teardown structure

--- bot/bot.go
+++ bot/bot.go
@@ -1,30 +1,101 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
+	"os/signal"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+var (
+	fnafAPI        = "localhost:9638"
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+)
 
+var session *discordgo.Session
+
+func init() { flag.Parse() }
+
+func init() {
 	// load env
 	env, err := godotenv.Read("../.env")
 	if err != nil {
 		log.Fatal("error: could not load .env file")
 	}
-	port := env["BOT_PORT"]
-	token := env["DISCORD_TOKEN"]
-	_ = []string{port}
 
-	// setup discord bot session
-	discord, err := discordgo.New("Bot " + token)
+	token := env["DISCORD_TOKEN"]
+
+	// setup session bot session
+	session, err = discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatal("error: could not create discord session: ", err)
 	}
+}
+
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "merge",
+			Description: "what follows?",
+		},
+	}
+	commandHandlers = map[string]func(session *discordgo.Session, interaction *discordgo.InteractionCreate){
+		"merge": func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+			session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "noob",
+				},
+			})
+		},
+	}
+)
+
+func init() {
+	session.AddHandler(func(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+		if handler, ok := commandHandlers[interaction.ApplicationCommandData().Name]; ok {
+			handler(session, interaction)
+		}
+	})
+}
+
+func main() {
+	session.AddHandler(func(session *discordgo.Session, ready *discordgo.Ready) {
+		log.Printf("logged in as: %v#%v", ready.User.Username, ready.User.Discriminator)
+	})
+	err := session.Open()
+	if err != nil {
+		log.Fatalf("cannot open the session: %v", err)
+	}
+	log.Println("adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, command := range commands {
+		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, "", command)
+		if err != nil {
+			log.Panicf("cannot create '%v' command: %v", command.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
 
 	// destruct discord session
-	defer discord.Close()
+	defer session.Close()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("press ctrl+c to exit")
+	<-stop
 
+	if *RemoveCommands {
+		log.Println("removing commands...")
+		for _, v := range registeredCommands {
+			err := session.ApplicationCommandDelete(session.State.User.ID, "", v.ID)
+			if err != nil {
+				log.Panicf("cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+	log.Println("gracefully shutting down.")
 }
